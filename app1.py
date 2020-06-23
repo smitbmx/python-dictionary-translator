62eae4638dda7d11023ee868bae6172e97ade391
Ivan Brygar <smitbmx@gmail.com>
2020-06-23 17:05:22 +0800

    Improved input experience.

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open('data.json'))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please double check it."
 
 word = input('Enter a word: ')
 
