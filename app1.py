1b56f763592ed0054646d4ca4a3ffcafe3dbb040
Ivan Brygar <smitbmx@gmail.com>
2020-06-23 20:55:11 +0800

    Added get_close_matches feature and a few additional checks.

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
@@ -6,9 +7,27 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[word.title()]
+    elif word.upper() in data: #in case user enters words like USA or NATO
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Press Y if yes, press N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input('Enter a word: ')
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == "list":
+    for item in output:
+        print(item)
+else:
+    print(output)
